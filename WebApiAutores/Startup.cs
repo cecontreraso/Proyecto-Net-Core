cdbcd25197c60f96addde0c4afa5591b505ff3a3
Cesar Contreras <cesar.contreras.or@gmail.com>
2023-08-21 16:16:43 -0400

    avance con el curso, modificacion de startup y program

---
+++ WebApiAutores/Startup.cs
@@ -0,0 +1,47 @@
+﻿
+
+namespace WebApiAutores
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) //Se inserta como una propiedad y eso genera una clas ede IConfigyration de get
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Aqui se van a configurar los servicios
+            //Se cortan los services de la parte de program
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) //env es el ambiente
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        
+
+        }
+
+    }
+}
