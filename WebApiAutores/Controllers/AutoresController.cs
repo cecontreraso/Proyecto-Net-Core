1b1c7ee5e46ca01796bf4efffb99c97193f79333
Cesar Contreras <cesar.contreras.or@gmail.com>
2023-08-23 21:28:34 -0400

    cambios y avances al prooyecto

---
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]//Atibuto, hace validaciones automaticas respecto a la data recibida
+    [Route("api/autores")]//Ruta de la API
+    public class AutoresController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>()
+            {
+
+                new Autor() { Id = 1, Nombre = "Cesar"},
+                new Autor() { Id = 2, Nombre = "Felipe"}
+            };
+                
+
+            
+        }
+
+    }
+}
