3b55bbbec143352ac98363a14ad84e04e67bfaf6
Cesar Contreras <cesar.contreras.or@gmail.com>
2023-08-23 22:52:33 -0400

    avance

---
+++ WebApiAutores/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)//constructor que se le pueden pasar distintas configuraciones, conexion string
+        {
+        }
+        //Ademas se pueden configurar las tablas
+        public DbSet<Autor> Autores { get; set; }
+    }
+}
